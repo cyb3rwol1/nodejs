a36b895d63c3e4ef3988a8b0f03291dc1e4c1fdc
yesley <cyb3rwolf1@gmail.com>
2018-05-31 20:05:26 -0400

    deze

--- aula_2_modulos_TCP/server.js
+++ aula_2_modulos_TCP/server.js
@@ -1,9 +1,18 @@
 var net = require('net');
 
+var connections = [];
+
+var trocaMensagem = function(mensagem){
+	connections.forEach(function(connection){
+		connection.write(mensagem.toString());
+	});
+};
+
 net.createServer(function(connection){
+	connections.push(connection);
 	connection.write('Seja bem vindo ao chat!');
-	connection.on('data',function(message){
-		console.log(message.toString());
+	connection.on('data',function(mensagem){
+		trocaMensagem(mensagem);
 	});
 }).listen(3000,function(){
 	console.log('Conectado Port:3000');
