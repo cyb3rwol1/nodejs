ddbac4cd1df4e711dc7e04075d734ba31f4793ab
yesley <cyb3rwolf1@gmail.com>
2018-07-23 05:20:51 -0400

    dev

--- crud_1/app.js
+++ crud_1/app.js
@@ -1,27 +1,14 @@
-var c = 0;
 var t;
-var timer_is_on = 0;
 
 function timedCount() {
-
-	if(c == 60) restartCount(); 
-    c = c + 1;
-    t = setTimeout(timedCount, 1000);
-}
-
-function startCount() {
 	console.log("Iniciando...");
-    if (!timer_is_on) {
-        timer_is_on = 1;
-        timedCount();
-    }
-}
-
-function restartCount() {
-    clearTimeout(t);
-    timer_is_on = 0;
-    startCount()
+	
     console.log("Finalizou!");
+    executeProcess();
 }
 
-startCount();
+function executeProcess() {
+    t = setTimeout(timedCount, 3000);
+}
+
+executeProcess();
