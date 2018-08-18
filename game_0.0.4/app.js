dab88cc3eb0d7139baa42f0eff865d10a6d68e21
yesley.miranda <yesley.miranda@redetendencia.com.br>
2018-08-18 12:05:22 -0400

    dev

--- game_0.0.4/app.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var app = express();
-var serv = require('http').Server(app);
-
-app.get('/', function(req, res){
-	res.sendFile(__dirname + '/client/index.html');
-});
-
-serv.listen(3000);
